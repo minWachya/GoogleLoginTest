65c34ba40ec9ff1245087f38805801a9dbce6da9
minwachya <minwachya@gmail.com>
2023-05-02 22:18:52 +0900

    [feat] 구글 로그인해서 이메일 받아오기 성공

--- app/src/main/java/com/example/googlelogingtest/MainActivity.kt
+++ app/src/main/java/com/example/googlelogingtest/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.googlelogingtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.example.googlelogingtest.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-class MainActivity : AppCompatActivity() {
+private const val RC_RIGN_IN = 300
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    companion object {
+        private val TAG = "mmm"
+    }
+
+    private lateinit var googleSignInOptions: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var googleAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //구글
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            //.requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        googleAuth = FirebaseAuth.getInstance()
+
+        initOnClickListener()
+    }
+
+    private fun initOnClickListener() {
+        binding.btnGoogleSignIn.setOnClickListener {
+            Log.d("mmm", "로그인 버튼 클릭")
+            googleSignIn()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_RIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                //Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                Log.d("mmm result:", account.email.toString())
+            }catch (e: ApiException){
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun googleSignIn(){
+        googleSignInClient.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_RIGN_IN)
     }
 }
